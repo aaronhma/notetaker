a3f4187755d8de580c08f63c01bcc80234dc7827
Aaron Ma <aaronhma@gmail.com>
2017-06-29 22:25:34 -0700

    Add Dashboard Message

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -4,6 +4,8 @@ import {
   Text,
   View,
   StyleSheet,
+  Image,
+  TouchableHighlight
 } from 'react-native';
 
 let styles = StyleSheet.create({
@@ -22,12 +24,54 @@ let styles = StyleSheet.create({
 });
 
 export default class Dashboard extends Component {
+  makeBackground(btn){
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+    if(btn === 0){
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1){
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
+  }
+  goToProfile(){
+    console.log('Going to Profile Page');
+  }
+  goToRepos(){
+    console.log('Going to Repos');
+  }
+  goToNotes(){
+  console.log('Going to Notes');
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text> This is the dashboard </Text>
-        <Text> {JSON.stringify(this.props.userInfo)} </Text>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image}/>
+        <TouchableHighlight
+            style={this.makeBackground(0)}
+            onPress={this.goToProfile.bind(this)}
+            underlayColor="#88D4F5">
+              <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(1)}
+            onPress={this.goToRepos.bind(this)}
+            underlayColor="#E39EBF">
+              <Text style={styles.buttonText}>View Repositories</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+            style={this.makeBackground(2)}
+            onPress={this.goToNotes.bind(this)}
+            underlayColor="#9BAAF3">
+              <Text style={styles.buttonText}>Take Notes</Text>
+        </TouchableHighlight>
       </View>
-    );
+    )
   }
 };
